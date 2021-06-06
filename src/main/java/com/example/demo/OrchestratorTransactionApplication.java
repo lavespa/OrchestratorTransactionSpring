24fadd0d019916ef2a1183d536699d3e4a638874
aintrier <alex_intrieri@yahoo.it>
2021-06-06 16:05:40 +0200

    add files orchestrator initial

--- src/main/java/com/example/demo/OrchestratorTransactionApplication.java
+++ src/main/java/com/example/demo/OrchestratorTransactionApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrchestratorTransactionApplication {
+	
+	//Example application transaction with spring
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrchestratorTransactionApplication.class, args);
